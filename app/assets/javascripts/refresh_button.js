028715ea982495f0a07e4353cdd2ab9431069fb7
gosborn <gosborn@gmail.com>
2015-07-29 14:04:39 -0400

    added dots variable

--- app/assets/javascripts/refresh_button.js
+++ app/assets/javascripts/refresh_button.js
@@ -1,15 +1,13 @@
 $(document).ready(function(){
 
+  var dots;
+
   $("#loading").hide();
   setInterval(addDots, 250);
 
   $('form.button_to input[type="submit"]').on("click",function(){
     $(this).hide();
-    
     $("#loading").show();
-    
-
-
   })
 })
 
