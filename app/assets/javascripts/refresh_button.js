6d40eb0245c74128d731e3affb3028fee6904679
gosborn <gosborn@gmail.com>
2015-07-29 11:32:27 -0400

    add loading button

--- app/assets/javascripts/refresh_button.js
+++ app/assets/javascripts/refresh_button.js
@@ -1,6 +1,25 @@
 $(document).ready(function(){
+
+  $("#loading").hide();
+  setInterval(addDots, 250);
+
   $('form.button_to input[type="submit"]').on("click",function(){
     $(this).hide();
+    
+    $("#loading").show();
+    
+
 
   })
 })
+
+function addDots(){
+  if(dots < 3){
+    $("#dots").append('.');
+      dots++;
+    }
+  else{
+    $("#dots").empty();
+      dots = 0;
+    }
+  };
