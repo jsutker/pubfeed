1026c9d01b52a5c4141f0a6c9b39382855ce72b8
jsutker <Joseph.Sutker@students.olin.edu>
2015-07-30 09:24:19 -0400

    made dead keywords clear and refactored article retrieving methods

--- app/models/email_automator.rb
+++ app/models/email_automator.rb
@@ -1,13 +1,25 @@
 class EmailAutomator
-
-  def self.clean_and_send
+  def self.clean
     Article.destroy_all
-    User.all.each do |user|
-      user.keywords.each do |keyword|
-        keyword.get_all_recent_abstracts(user)
+    Keyword.all.each do |keyword|
+      if keyword.users.empty?
+        keyword.destroy
       end
+    end
+  end
+  def self.retrieve
+    Keyword.all.each do |keyword|
+      keyword.get_all_recent_abstracts
+    end
+  end
+  def self.send
+    User.all.each do |user|
       UserMailer.daily_result_email(user).deliver_now
     end
   end
-
+  def self.clean_and_send
+    self.clean
+    self.retrieve
+    self.send
+  end
 end
