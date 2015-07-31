9175f182e2223fd34be1bbbb28a116408b4f4058
jsutker <Joseph.Sutker@students.olin.edu>
2015-07-31 14:18:59 -0400

    refactored ArticleController#update

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -28,8 +28,7 @@ class ArticlesController < ApplicationController
     articles = current_user.articles
     @articles = articles
     article = Article.find(params[:id])
-    f = Favorite.new(title: article.title, abstract: article.abstract, id_from_json: article.id_from_json, authors: article.authors, journal: article.journal, user_id: current_user.id )
-    f.save
+    f = Favorite.create(title: article.title, abstract: article.abstract, id_from_json: article.id_from_json, authors: article.authors, journal: article.journal, user_id: current_user.id )
     respond_to do |format|
       format.js
     end
